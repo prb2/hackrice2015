769d75d6f5f4ae7af186625b33f1877d5588031c
Terry Lin <t.lin103@gmail.com>
2015-01-31 15:18:37 -0600

    Testing out Activity Pages

---
+++ DrinkAndPay/src/hackrice/team/drinkup/EventPage.java
@@ -0,0 +1,15 @@
+package hackrice.team.drinkup;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Terry on 1/31/2015.
+ */
+public class EventPage extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_page);
+    }
+}
