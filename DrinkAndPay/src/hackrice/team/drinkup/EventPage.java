d5c121febef39bcba4b1c3878bd4984cfac6b89f
Terry Lin <t.lin103@gmail.com>
2015-01-31 16:32:26 -0600

    Testing out Activity Pages - Intent

--- DrinkAndPay/src/hackrice/team/drinkup/EventPage.java
+++ DrinkAndPay/src/hackrice/team/drinkup/EventPage.java
@@ -1,7 +1,9 @@
 package hackrice.team.drinkup;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by Terry on 1/31/2015.
@@ -12,4 +14,10 @@ public class EventPage extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_page);
     }
+
+    public void BackToMain(View view){
+        Intent i = new Intent(EventPage.this,MainActivity.class);
+        startActivity(i);
+    }
+
 }
